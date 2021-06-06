8efd7ee7a1feb1f0830a1846f8b010a3641b8b92
onurtaskirancom <taskiranonur9@gmail.com>
2021-06-06 05:15:54 +0300

    Added breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
@@ -15,6 +16,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
