0a5b16153b323212649e4f5dd4838b695b2990d6
onurtaskirancom <taskiranonur9@gmail.com>
2021-06-16 03:21:41 +0300

    Displayed the basket item count in the navbar

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,7 +20,8 @@ export class BasketService {
     return this.http.get(this.baseUrl + 'basket?id=' + id)
       .pipe(
         map((basket: IBasket) => {
-          this.basketSource.next(basket);   
+          this.basketSource.next(basket);
+          console.log(this.getCurrentBasketValue());
         })
       );
   }
