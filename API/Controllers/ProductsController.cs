306b07f96a0bc278cf5bd8ee57b791dc3cec8005
onurtaskirancom <taskiranonur9@gmail.com>
2021-08-23 03:11:06 +0300

    Testing the caching

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -33,6 +33,7 @@ namespace API.Controllers
 
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery]ProductSpecParams productParams)
@@ -52,6 +53,7 @@ namespace API.Controllers
                 productParams.pageSize, totalItems, data));
         }
 
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -66,12 +68,14 @@ namespace API.Controllers
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _productBrandRepo.ListAllAsync());
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
